9e8ec8dc5dbd62f0b779806a822eb7b2767b0fcc
Matěj Foltín <foltinmatej@gmail.com>
2022-11-22 21:03:50 +0100

    Documentation for Box class

--- AlgorithmII-Project/Box.h
+++ AlgorithmII-Project/Box.h
@@ -2,12 +2,24 @@
 #include <string>
 #include <sstream>
 
+//! Class representing single box.
 class Box
 {
 public:
-	int height, width, length;
+	int height; //!< Height of the box.
+	int width; //!< Width of the box.
+	int length; //!< Length of the box.
 
+	//! Class constructor
+	/*!
+	 * Construct object by passing height, width, length represented by integer.
+	 */
 	Box(int, int, int);
+
+	//! Transform the values to string format
+	/*!
+	 * Returns standardized output in format "width x length x height"
+	 */
 	std::string ToString();
 };
 
